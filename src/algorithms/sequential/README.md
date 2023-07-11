275afa2574c7e5565620dbc5cf3fe9d4fea8cea6
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-07-11 01:04:41 -0500

    feat: sequential

---
+++ src/algorithms/sequential/README.md
@@ -0,0 +1,16 @@
+# mpi
+
+## Build
+```shell
+make
+```
+
+## Run
+```shell
+make run -e n={n} m={m} nPrime={nPrime}
+```
+
+## Clean
+```shell
+make clean
+```
--- src/algorithms/sequential/main.cpp
+++ src/algorithms/sequential/main.cpp
@@ -1,24 +1,31 @@
 #include <chrono>
 #include <set>
-#include "../../utils/functions/isWellColored.h"
 #include "../../generator/GraphGenerator.h"
+#include "../../utils/functions/isWellColored.h"
 
 using namespace std::chrono;
 
 int main(int argc, char** argv) {
-    int numVertices = 1e4;
-    GraphGenerator graph(numVertices);
-    graph.generateGraph(6245000, 8, 1, 42);
-    auto adjMatrix = graph.getGraph();
-    graph.validateGraph();
+    GraphGenerator* graphGenerator = new GraphGenerator();
+    if (argc < 4) {
+        std::cerr << "Insufficient arguments!" << std::endl;
+        return 1;
+    }
+    lli n = std::stoll(argv[1]);
+    lli m = std::stoll(argv[2]);
+    lli nPrime = std::stoll(argv[3]);
+    graphGenerator->loadIfExistsOrGenerateNewGraph(n, m, nPrime);
+
+    graphGenerator->validateGraph();
+    auto adjMatrix = graphGenerator->getGraph();
 
     // sequential coloring
     auto start = high_resolution_clock::now();
-    int* colors = new int[numVertices];
-    for (int i = 0; i < numVertices; i++) {
+    lli* colors = new lli[n];
+    for (int i = 0; i < n; i++) {
         // Get the colors of adjacent vertices
         std::set<int> C;
-        for (int j = 0; j < numVertices; j++) {
+        for (int j = 0; j < n; j++) {
             if (adjMatrix[i][j] && colors[j] != 0) {
                 C.insert(colors[j]);
             }
@@ -34,11 +41,11 @@ int main(int argc, char** argv) {
         colors[i] = smallestColor;
     }
 
-    isWellColored(colors, adjMatrix, numVertices);
+    isWellColored(colors, n, adjMatrix);
 
     // get chromatic number
     int chromaticNumber = 0;
-    for (int i = 0; i < numVertices; i++) {
+    for (int i = 0; i < n; i++) {
         if (colors[i] > chromaticNumber) {
             chromaticNumber = colors[i];
         }
