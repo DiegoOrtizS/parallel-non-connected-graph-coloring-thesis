c79336892a08587753bdae043c482b2e800a734c
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-07-11 04:40:12 -0500

    fix: hybrid

--- src/algorithms/hybrid/main.cpp
+++ src/algorithms/hybrid/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &processId);
 
     GraphGenerator *graphGenerator = new GraphGenerator();
-
+    int threads;
     if (processId == 0) {
         if (argc < 5) {
             std::cerr << "Insufficient arguments!" << std::endl;
@@ -19,17 +19,18 @@ int main(int argc, char** argv) {
         lli n = std::stoll(argv[1]);
         lli m = std::stoll(argv[2]);
         lli nPrime = std::stoll(argv[3]);
-        int threads = std::stoi(argv[4]);
-        omp_set_num_threads(threads);
+        threads = std::stoi(argv[4]);
         graphGenerator->loadIfExistsOrGenerateNewGraph(n, m, nPrime);
     }
-    
+
+    MPI_Bcast(&threads, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    omp_set_num_threads(threads);
     auto start = MPI_Wtime();
     ColoringResult result = coloringHybrid(processId, graphGenerator->getN(), graphGenerator->getGraph(), coloringOMP);
     auto stop = MPI_Wtime();
 
     if (processId == 0) {
-        isWellColored(result.colors, graphGenerator->getN(), graphGenerator->getGraph());
+        isWellColored(result.colors, graphGenerator->getN(), graphGenerator->getGraph(), result.labels);
         std::cout << "Chromatic number: " << result.chromaticNumber << std::endl;
         std::cout << "Total time: " << stop - start << " seconds" << std::endl;
         
