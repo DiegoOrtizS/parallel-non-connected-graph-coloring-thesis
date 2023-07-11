829ba3fb0b126934a1a0ddd164abf36dd2af2bd6
DiegoOrtizS <diego.ortiz@utec.edu.pe>
2023-07-10 23:31:08 -0500

    fix: reduce total time

--- src/algorithms/mpi/coloringMPI.h
+++ src/algorithms/mpi/coloringMPI.h
@@ -5,8 +5,8 @@
 #include "connectedComponents.h"
 #include "coloringAlgorithms.h"
 
-void coloringComponents(int submatrixSize, const std::vector<lli> &componentData, ColoringResult (*coloringAlgorithm)(lli, lli**), std::vector<lli> &colors, std::vector<lli> &labels, lli &chromaticNumber) {
-    int currentIndex = 0;
+void coloringComponents(const std::vector<lli> &componentData, ColoringResult (*coloringAlgorithm)(lli, lli**), std::vector<lli> &colors, std::vector<lli> &labels, lli &chromaticNumber) {
+    int currentIndex = 0, submatrixSize;
     while (currentIndex < componentData.size()) {
         submatrixSize = componentData[currentIndex];
         currentIndex++;
@@ -42,7 +42,7 @@ void coloringComponents(int submatrixSize, const std::vector<lli> &componentData
 ColoringResult coloringMPI(const int &processId, const lli &n, lli **graph, ColoringResult (*coloringAlgorithm)(lli, lli**)) {
     std::vector<lli> colors, labels;
     lli chromaticNumber = 0;
-    int submatrixSize, processSize, sumSubmatrixSize;
+    int processSize, sumSubmatrixSize;
     MPI_Request request;
     MPI_Status status;
     MPI_Comm_size(MPI_COMM_WORLD, &processSize);
@@ -68,23 +68,6 @@ ColoringResult coloringMPI(const int &processId, const lli &n, lli **graph, Colo
             displacements[i] = displacements[i - 1] + sendCounts[i - 1];
         }
 
-        std::vector<lli> componentData;
-        sumSubmatrixSize = 0;
-        for (int j = displacements[0]; j < displacements[0] + sendCounts[0]; j++) {
-            std::vector<lli> c = components[j];
-            lli componentSize = c.size();
-            sumSubmatrixSize += componentSize;
-            componentData.push_back(componentSize);
-            componentData.insert(componentData.end(), c.begin(), c.end());
-            for (lli k = 0; k < componentSize; k++) {
-                for (lli l = 0; l < componentSize; l++) {
-                    componentData.push_back(graph[c[k]][c[l]]);
-                }
-            }
-        }
-
-        coloringComponents(submatrixSize, componentData, coloringAlgorithm, colors, labels, chromaticNumber);
-
         for (int i = 1; i < processSize; i++) {
             std::vector<lli> sendComponentData;
             int sendSum = 0;
@@ -104,6 +87,24 @@ ColoringResult coloringMPI(const int &processId, const lli &n, lli **graph, Colo
             MPI_Send(sendComponentData.data(), sendComponentData.size(), MPI_LONG_LONG_INT, i, 0, MPI_COMM_WORLD);
             MPI_Send(&sendSum, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
+
+        std::vector<lli> componentData;
+        sumSubmatrixSize = 0;
+        for (int j = displacements[0]; j < displacements[0] + sendCounts[0]; j++) {
+            std::vector<lli> c = components[j];
+            lli componentSize = c.size();
+            sumSubmatrixSize += componentSize;
+            componentData.push_back(componentSize);
+            componentData.insert(componentData.end(), c.begin(), c.end());
+            for (lli k = 0; k < componentSize; k++) {
+                for (lli l = 0; l < componentSize; l++) {
+                    componentData.push_back(graph[c[k]][c[l]]);
+                }
+            }
+        }
+
+        coloringComponents(componentData, coloringAlgorithm, colors, labels, chromaticNumber);
+
     }
     else { // Processes other than 0
         int recvCount;
@@ -114,7 +115,7 @@ ColoringResult coloringMPI(const int &processId, const lli &n, lli **graph, Colo
         MPI_Recv(receivedComponent.data(), recvCount, MPI_LONG_LONG_INT, 0, 0, MPI_COMM_WORLD, &status);
         MPI_Recv(&sumSubmatrixSize, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
 
-        coloringComponents(submatrixSize, receivedComponent, coloringAlgorithm, colors, labels, chromaticNumber);
+        coloringComponents(receivedComponent, coloringAlgorithm, colors, labels, chromaticNumber);
     }
 
     lli maxChromaticNumber;
